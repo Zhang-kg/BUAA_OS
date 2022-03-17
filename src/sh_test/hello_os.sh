9a2a37ec93ad7f38201136b87212badbaf9f3ce6
GitLab <gitlab@git.os.buaa.edu.cn>
2022-03-17 18:38:53 +0800

    Initial lab1

--- src/sh_test/hello_os.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-touch $2
-cat $1 | head -n 8 | tail -n 1 > $2
-cat $1 | head -n 32 | tail -n 1 >> $2
-cat $1 | head -n 128 | tail -n 1 >> $2
-cat $1 | head -n 512 | tail -n 1 >> $2
-cat $1 | head -n 1024 | tail -n 1 >> $2
-
-
