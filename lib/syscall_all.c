26d9940ca6ff3d8efaef69d9a829c6597f9cdbb3
Zhang_kg <20373067@buaa.edu.cn>
2022-05-08 16:54:39 +0800

    lab4-1 debug2

--- lib/syscall_all.c
+++ lib/syscall_all.c
@@ -153,7 +153,7 @@ int sys_mem_alloc(int sysno, u_int envid, u_int va, u_int perm)
     if (va >= UTOP) return -E_INVAL;
     	
     // page alloc and map
-    if ((ret = envid2env(envid, &env, 0)) < 0) return ret;
+    if ((ret = envid2env(envid, &env, 1)) < 0) return ret;
     if ((ret = page_alloc(&ppage)) < 0) return ret;
     if ((ret = page_insert(env -> env_pgdir, ppage, va, perm)) < 0) return ret;
   
