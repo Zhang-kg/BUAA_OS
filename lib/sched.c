93a2c2b7b97c9bcca4bdeb2ad28114547e40ae51
Zhang_kg <20373067@buaa.edu.cn>
2022-05-10 13:11:47 +0800

    debug lab4-2

--- lib/sched.c
+++ lib/sched.c
@@ -32,7 +32,7 @@ void sched_yield(void)
 				LIST_REMOVE(e, env_sched_link);
 			} else if (e -> env_status == ENV_NOT_RUNNABLE) {
 				LIST_REMOVE(e, env_sched_link);
-				//LIST_INSERT_TAIL(&env_sched_list[1 - point], e, env_sched_link);
+				LIST_INSERT_TAIL(&env_sched_list[1 - point], e, env_sched_link);
 			} else {
 				count = e -> env_pri;
 				break;
