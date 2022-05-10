21934b0a0d5ff697c89d90c83903e23ef00d1002
Zhang_kg <20373067@buaa.edu.cn>
2022-05-10 13:01:53 +0800

    debug lab4-2

--- lib/syscall_all.c
+++ lib/syscall_all.c
@@ -284,9 +284,8 @@ int sys_set_env_status(int sysno, u_int envid, u_int status)
 	int ret;
 	if (status != ENV_RUNNABLE && status != ENV_NOT_RUNNABLE && status != ENV_FREE) return -E_INVAL;
     if ((ret = envid2env(envid, &env, 0)) < 0) return -E_INVAL;
-    int flag = (env -> env_status == ENV_FREE);
 	env -> env_status = status;
-    if (env -> env_status == ENV_RUNNABLE && flag) LIST_INSERT_HEAD(env_sched_list, env, env_sched_link);
+    if (env -> env_status == ENV_RUNNABLE) LIST_INSERT_HEAD(env_sched_list, env, env_sched_link);
 	return 0;
 	//	panic("sys_env_set_status not implemented");
 }
