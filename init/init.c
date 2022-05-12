6f9b864c03a94eedc7b20b5e0cb27453f57641ea
Zhang_kg <20373067@buaa.edu.cn>
2022-05-12 15:01:14 +0800

    extra1

--- init/init.c
+++ init/init.c
@@ -5,28 +5,59 @@
 #include <kclock.h>
 #include <trap.h>
 
+extern u_int test1(char *p, u_int offset);
+extern u_int test2(char *p, u_int offset);
 
+void test() {
+        char a[100] = {5, 4, 3, 2, 1};
+        int i = 0;
+        // lw -> lh
+        i = test1(a, 2);
+        printf("%08x\n", i);
+        // lh -> lb
+        i = test1(a, 3);
+        printf("%08x\n", i);
+        // lw -> lb
+        i = test2(a, 1);
+        printf("%08x\n", i);
+}
 
 void mips_init()
 {
-	printf("init.c:\tmips_init() is called\n");
-	mips_detect_memory();
+        printf("init.c:\tmips_init() is called\n");
+        mips_detect_memory();
+
+        mips_vm_init();
+        page_init();
+
+        env_init();
+
+        trap_init();
+        kclock_init();
+        test();
+        *((volatile char *) 0xB0000010);
+}
+
+//void mips_init()
+//{
+//	printf("init.c:\tmips_init() is called\n");
+//	mips_detect_memory();
 	
-	mips_vm_init();
-	page_init();
+//	mips_vm_init();
+//	page_init();
 	
-	env_init();
+//	env_init();
     
     // for lab3-2-exam local test
-    ENV_CREATE_PRIORITY(user_A, 2);
-    ENV_CREATE_PRIORITY(user_B, 1);	
+//    ENV_CREATE_PRIORITY(user_A, 2);
+//    ENV_CREATE_PRIORITY(user_B, 1);	
 
-	trap_init();
-	kclock_init();
-	panic("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^");
-	while(1);
-	panic("init.c:\tend of mips_init() reached!");
-}
+//	trap_init();
+//	kclock_init();
+//	panic("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^");
+//	while(1);
+//	panic("init.c:\tend of mips_init() reached!");
+//}
 
 
 //void mips_init()
