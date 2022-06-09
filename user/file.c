192e06e64cefae039d8f258a1b57f96f7074ee92
Zhang_kg <20373067@buaa.edu.cn>
2022-06-09 15:06:11 +0800

    try1

--- user/file.c
+++ user/file.c
@@ -48,16 +48,22 @@ open(const char *path, int mode)
 
 	// Step 3: Set the start address storing the file's content. Set size and fileid correctly.
 	// Hint: Use fd2data to get the start address.
+	if (mode & O_ALONE) {
+		syscall_mem_map(0, (void *)fd, 0, (void *)fd, (PTE_V | PTE_R));
+	}
 	ffd = (struct Filefd*)fd;
 	va = fd2data(fd);
 	fileid = ffd -> f_fileid;
 	size = ffd -> f_file.f_size;
-
+	if (mode & O_APPEND)	fd->fd_offset = size;
 	// Step 4: Alloc memory, map the file content into memory.
 	for (i = 0; i < size; i += BY2PG) {
 		if ((r = fsipc_map(fileid, i, va + i)) < 0) {
 			writef("cannot map the file.\n");
 			return r;
+		}
+		if (mode & O_ALONE) {
+			syscall_mem_map(0, va + i, 0, va + i, (PTE_V | PTE_R | PTE_COW));
 		}
 	}
 
@@ -188,7 +194,7 @@ file_write(struct Fd *fd, const void *buf, u_int n, u_int offset)
 			return r;
 		}
 	}
-
+	//writef("offset = %d\n", offset);
 	// Write the data
 	user_bcopy(buf, (char *)fd2data(fd) + offset, n);
 	return n;
--- user/fstest.c
+++ user/fstest.c
@@ -1,5 +1,92 @@
 #include "lib.h"
 
+
+void umain()
+{
+    int fdnum;
+    char buf[200];
+    fdnum = open("/created_file", O_RDWR|O_CREAT);
+    fwritef(fdnum, "test create");
+    close(fdnum);
+    fdnum = open("/created_file", O_RDWR);
+    read(fdnum, buf, 150);
+    writef("read from new file: %s\n", buf);
+    while(1);
+}
+
+/* expected output:
+==================================================================
+read from new file: test create
+==================================================================
+*/
+
+
+/*
+#include "lib.h"
+
+
+void umain()
+{
+    int r, fdnum, n;
+    char buf[200];
+    fdnum = open("/newmotd", O_RDWR | O_ALONE);
+    if ((r = fork()) == 0) {
+	    n = read(fdnum, buf, 5);
+	    writef("[child] buffer is \'%s\'\n", buf);
+    } else {
+	    n = read(fdnum, buf, 5);
+	    writef("[father] buffer is \'%s\'\n", buf);
+    }
+    while(1);
+}
+*/
+/* expected output:
+==================================================================
+[father] buffer is 'This '
+[child] buffer is 'This '
+==================================================================
+*/
+
+/*
+#include "lib.h"
+
+
+void umain()
+{
+    int r, fdnum, n;
+    char buf[200];
+    if ((r = open("/newmotd", O_RDWR | O_APPEND) < 0)) {
+	    user_panic("open /motd: %d\n", r);
+    }
+    fdnum = r;
+    if (r = fwritef(fdnum, "test append") < 0) {
+	    user_panic("fwritef %d\n", r);
+    }
+    close(fdnum);
+    if ((r = open("/newmotd", O_RDWR) < 0)) {
+	    user_panic("open /motd: %d\n", r);
+    }
+    fdnum = r;
+    if ((n = read(fdnum, buf, 150)) < 0) {
+	    writef("read %d\n", n);
+	    return;
+    }
+    close(fdnum);
+    writef("\n%s\n", buf);
+    while (1);
+}
+*/
+
+
+/* expected output:
+==================================================================
+This is a different massage of the day!
+test append
+==================================================================
+*/
+
+/*#include "lib.h"
+
 static char *msg = "This is the NEW message of the day!\n\n";
 static char *diff_msg = "This is a different massage of the day!\n\n";
 
@@ -69,4 +156,4 @@ void umain()
         }
 }
 
-
+*/
